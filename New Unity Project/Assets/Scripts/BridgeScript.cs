1a6bad5749dfed2ba09db6933e1a37bd5d3fc869
Michael16219 <61007828+Michael16219@users.noreply.github.com>
2020-04-23 15:10:32 +0800

    23/04/2020

---
+++ New Unity Project/Assets/Scripts/BridgeScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation(this.transform.up, Time.deltaTime
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ClimbTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbTrigger : MonoBehaviour
+{
+    public bool PlayerClimb = false;
+    public float moveSpeed = 5f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerClimb = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerClimb = false;
+        }
+    }
+    void Update()
+    {
+        while (bool.PlayerClimb = true)
+        {
+            GetComponent<PlayerController>();
+            if (Input.GetKey(KeyCode.W) == true) {transform.position += transform.up * Time.deltaTime * this.moveSpeed; }
+        }
+    }
+}
