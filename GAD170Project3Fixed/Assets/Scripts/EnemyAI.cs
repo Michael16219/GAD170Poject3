b9e322e19892fa7dc7a5a4a444e4150d54ca425f
bloodmurderer1 <61007754+bloodmurderer1@users.noreply.github.com>
2020-04-29 21:19:20 +0800

    EnemyAI

---
+++ GAD170Project3Fixed/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent enemy;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination = player.transform.position;
+    }
+}
--- GAD170Project3Fixed/Assets/Scripts/ResetLevel.cs
+++ GAD170Project3Fixed/Assets/Scripts/ResetLevel.cs
@@ -14,6 +14,6 @@ public class ResetLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) == true) { SceneManager.LoadScene(0); }
+        if (Input.GetKeyDown(KeyCode.P) == true) { SceneManager.LoadScene(0); }
     }
 }
